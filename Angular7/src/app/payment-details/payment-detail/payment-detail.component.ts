08a20c85646d5ff719abf58627fc4726615303c9
Razu <jahangir8988@gmail.com>
2019-04-29 19:08:20 +0600

    initial commit

---
+++ Angular7/src/app/payment-details/payment-detail/payment-detail.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-payment-detail',
+  templateUrl: './payment-detail.component.html',
+  styles: []
+})
+export class PaymentDetailComponent implements OnInit {
+
+  constructor(private service: PaymentDetailService, private toastr: ToastrService) { }
+
+  ngOnInit() {
+    this.resetForm();
+  }
+
+  resetForm(form?: NgForm) {
+    if (form != null)
+      form.resetForm();
+    this.service.formData = {
+      PMId: 0,
+      CardOwnerName: '',
+      CardNumber: '',
+      ExpirationDate: '',
+      CVV: ''
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    if (this.service.formData.PMId == 0)
+      this.insertRecord(form);
+    else
+      this.updateRecord(form);
+  }
+
+  insertRecord(form: NgForm) {
+    this.service.postPaymentDetail().subscribe(
+      res => {
+        this.resetForm(form);
+        this.toastr.success('Submitted Succsessfully', 'Payment Detail Register');
+        this.service.refreshList();
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
+  updateRecord(form: NgForm) {
+    this.service.putPaymentDetail().subscribe(
+      res => {
+        this.resetForm(form);
+        this.toastr.info('Submitted Succsessfully', 'Payment Detail Register');
+        this.service.refreshList();
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
+
+}
